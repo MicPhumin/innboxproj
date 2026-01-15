8bd7a52ee15931d5bcb5871220a995bab25a0865
MicPhumin <114295792+MicPhumin@users.noreply.github.com>
2026-01-15 18:10:03 +0700

    del comma

--- src/screen/AdminScreen.tsx
+++ src/screen/AdminScreen.tsx
@@ -86,7 +86,6 @@ const AdminScreen = () => {
             onChange={onChange}
             checked={isActive === "true" ? true : false}
           ></Checkbox>
-          ,
         </>
       ),
     },
